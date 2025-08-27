0d2b1d28618969fa260b18f15c7f5070c2077c3e
RepbrasilMauricio <suporte.mauricio@repbrasil.salvador.br>
2025-08-27 20:59:46 -0300

    Update

--- src/main/java/com/mauriciojdk/dslist/controllers/GameListController.java
+++ src/main/java/com/mauriciojdk/dslist/controllers/GameListController.java
@@ -33,6 +33,7 @@ public class GameListController {
         return result;
     }
 
+
     @PostMapping(value = "/{listId}/replacement")
     public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body){
         gameListService.move(listId, body.getSourceIndex(),  body.getDestinationIndex());
